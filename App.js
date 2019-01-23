3c9879807c7be7edf4c2d045fb2561d02a0a5eda
ChhXin <chh.xin@gmail.com>
2019-01-23 18:30:39 +0800

    fix

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +16,18 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!8888888888</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{ width: "100%", height: 50, backgroundColor: 'powderblue' }} />
         <Text style={styles.instructions}>{instructions}</Text>
+        <View style={{ width: "100%", height: 50, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
