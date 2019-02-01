5c59388e8facaffdfd3560a57a5149fe07fe5636
ChhXin <chh.xin@gmail.com>
2019-02-01 09:50:48 +0800

    fix code

--- android/app/src/main/java/com/rnapp/MainActivity.java
+++ android/app/src/main/java/com/rnapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rnapp;
+// package com.swmansion.gesturehandler.react.example;
 
 import com.facebook.react.ReactActivity;
+
+// import com.facebook.react.ReactActivityDelegate;
+// import com.facebook.react.ReactRootView;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "RnApp";
     }
+    
+    // @Override
+    // protected ReactActivityDelegate createReactActivityDelegate() {
+    //     return new ReactActivityDelegate(this, getMainComponentName()) {
+    //     @Override
+    //     protected ReactRootView createRootView() {
+    //         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+    //     }
+    //     };
+    // }
 }
